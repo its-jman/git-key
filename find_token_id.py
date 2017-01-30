0c533507cc1c9f4640d7e396bae391e92f75f833
Josh Manning <jbmmhk@ksu.edu>
2017-01-30 14:44:56 -0600

    Converted script to Python.

--- find_token_id.py
+++
@@ -1,9 +0,0 @@
-import sys
-import json
-
-val = json.load(sys.stdin)
-type(val)
-keys = filter(lambda key: key['app']['name'] == sys.argv[1], val)
-
-if len(keys):
-    print(keys[0]['id'])
--- update-key.py
+++ update-key.py
@@ -1,25 +1,115 @@
 #!/usr/bin/python3
 import sys
 import requests
+import subprocess
 
-from getpass import getpass
+from os import path
+from time import sleep
+from socket import gethostname
+from getpass import getpass, getuser
 
 if len(sys.argv) != 2:
     raise ValueError('Script should be run with the following parameters:\n \
                         \t./update-key.sh [user]')
 user = sys.argv[1]
+# Two-Factor Auth code
 auth = ''
+pat_name = 'Update SSH key'
+# Corresponding method names and method functions.
 methods = {
     'GET': requests.get,
     'POST': requests.post,
     'DELETE': requests.delete
 }
+invalid_credential_messages = [
+    'Bad credentials',
+    'Must specify two-factor authentication OTP code.'
+]
+
+
+def update_ssh_key(access_token, ssh_key_name):
+    print('\nFinding existing SSH keys on GitHub. ')
+    user_ssh_keys = request_url_by_token(access_token, url='https://api.github.com/user/keys', method_name='GET').json()
+    old_ssh = list(filter(lambda item: item.get('title') == ssh_key_name, user_ssh_keys))
+    # If the key already exists, delete it.
+    if len(old_ssh):
+        old_ssh_id = old_ssh[0].get('id')
+
+        print('\nDeleting old SSH key, ID: (%s)\n' % old_ssh_id)
+        request_url_by_token(
+            access_token,
+            url='https://api.github.com/user/keys/%s' % old_ssh_id,
+            method_name='DELETE'
+        )
+    ssh_path = path.expanduser('~/.ssh/id_rsa')
+
+    subprocess.run(['ssh-keygen', '-t', 'rsa', '-b', '4096', '-f', ssh_path, '-N', ''])
+    subprocess.run(['ssh-add', ssh_path])
+    with open(ssh_path + '.pub', 'r') as ssh_file:
+        ssh_value = ssh_file.readline()
+
+    print('\nAdding the created token to your GitHub account. ')
+    response = request_url_by_token(
+        access_token,
+        url='https://api.github.com/user/keys',
+        method_name='POST',
+        headers={
+            'Content-Type': 'application/json'
+        },
+        data={
+            'title': ssh_key_name,
+            'key': ssh_value
+        }
+    )
+
+    if response.json().get('verified'):
+        print('Success!\n')
+    else:
+        print('Something unexpected happened. ')
+        print(response)
+
+
+def remove_pat(pat_id):
+    if pat_id is None:
+        print('Invalid PAT id in remove_pat.')
+        sys.exit(1)
+    print('Removing Personal Access token, ID: (%s)' % pat_id)
+    return request_url_by_credentials(
+        url='https://api.github.com/authorizations/%s' % pat_id,
+        method_name='DELETE'
+    )
+
+
+def remove_existing_pat():
+    """
+    Get existing tokens
+    Find the one with 'Update SSH key' as the name
+    Remove that PAT
+    :return:
+    """
+    print('\nThe \'%s\' PAT already exists, attempting to remove. ' % pat_name)
+    existing_tokens = request_url_by_credentials(url='https://api.github.com/authorizations', method_name='GET').json()
+    old_token = list(filter(lambda item: item.get('note') == pat_name, existing_tokens))[0]
+
+    remove_pat(old_token.get('id', None))
+
+
+def url_function(f):
+    def out_f(*args, **kwargs):
+        if kwargs.get('headers', None) is None:
+            kwargs.update({
+                'headers': {}
+            })
+        if kwargs.get('data', None) is not None:
+            kwargs['json'] = kwargs.pop('data')
+        return f(*args, **kwargs)
+    return out_f
 
 
 def get_auth_code():
     global auth
     if auth != '':
-        new_auth = input('Two-Factor Authorization ($auth): ')
+        new_auth = input('Two-Factor Authorization (%s): ' % auth)
     else:
         new_auth = input('Two-Factor Authorization: ')
 
@@ -28,49 +118,89 @@ def get_auth_code():
     return auth
 
 
-def request_url(url, method_name, headers=None, data=None, request_auth=None):
-    method = methods[method_name]
-    result = method(url, headers=headers, json=data, auth=request_auth)
+def request_url(**kwargs):  # url, method_name, headers=None, data=None, request_auth=None):
+    method = methods[kwargs.pop('method_name')]
+    response = method(**kwargs)
 
-    return result
-
-
-def request_url_by_token():
-    pass
+    return response
 
 
-def request_url_by_credentials(*args, **kwargs):
-    if False:  # BAD CREDENTIALS
-        pass
+@url_function
+def request_url_by_token(access_token, **kwargs):
+    """
+    Requests URL by Personal Access Token provided by initial API request.
+    :return:
+    """
+    # Always add the credentials to the request.
+    kwargs.update({
+        'auth': (user, access_token)
+    })
 
-print('Creating an auth token for further requests.')
-create_token_response = request_url(
-    url='https://api.github.com/authorizations',
-    method_name='POST',
-    data={
-        'scopes': ['admin:public_key'],
-        'note': 'Update ssh key'
-    },
-    headers={
-        'X-GitHub-OTP': get_auth_code(),
-        'Content-Type': 'application/json'
-    },
-    request_auth=(user, getpass('Password for [%s]: ' % user))
-)
+    response = request_url(**kwargs)
 
-ctr_json = create_token_response.json()
-
-print(ctr_json)
-auth_token = ctr_json.get('token', None)
-
-if not auth_token:
+    return response
 
 
+@url_function
+def request_url_by_credentials(**kwargs):
+    response = None
+    while response is None:
+        # Always add the Two-Factor header.
+        kwargs['headers'].update({
+            'X-GitHub-OTP': get_auth_code()
+        })
+        # Always add the credentials to the request.
+        kwargs.update({
+            'auth': (user, getpass('Password for [%s]: ' % user))
+        })
 
-# token_response=`curl -sS \
-#  -u '$1' \
-#  -H 'X-GitHub-OTP:'$auth'' \
-#  -X POST \
-#  -H 'Content-Type: application/json' \
-#  -d '{'scopes': ['admin:public_key'], 'note': 'Update ssh key'}' \
-#  'https://api.github.com/authorizations'`
+        response = request_url(**kwargs)
+
+        # DELETE responses do not contain a body, and crash upon calling .json()
+        if response.text:
+            response_json = response.json()
+
+            # Continue loop if entered bad credentials
+            if type(response_json) == dict:
+                if response_json.get('message') in invalid_credential_messages:
+                    response = None
+    return response
+
+
+def main():
+    print('Creating a Personal Access Token for further requests.')
+    create_token_response = request_url_by_credentials(
+        url='https://api.github.com/authorizations',
+        method_name='POST',
+        data={
+            'scopes': ['admin:public_key'],
+            'note': pat_name
+        },
+        headers={
+            'Content-Type': 'application/json'
+        }
+    )
+
+    ctr_json = create_token_response.json()
+
+    pat_token = ctr_json.get('token', None)
+    if pat_token:
+        # Personal Access Token ID, used for removal at the end.
+        pat_token_id = ctr_json.get('id')
+        update_ssh_key(pat_token, '%s@%s' % (getuser(), gethostname()))
+        sleep(600)
+        remove_pat(pat_token_id)
+    else:
+        errors = ctr_json.get('errors', None)
+        if errors:
+            code = errors[0].get('code')
+            if code == 'already_exists':
+                remove_existing_pat()
+                print('Removed existing PAT, please try running the program again. ')
+        else:
+            print('Creation of Personal Access Token failed. ')
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
